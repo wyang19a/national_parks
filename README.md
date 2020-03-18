7236b480f9bb8161950a0743547cfe22bd9e65ea
wyang19a <wyang19a@gmail.com>
2020-03-18 11:27:39 -0400

    Edit readme, add image for edit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # US National Parks
 
-for interview
+Coding assessment for interview.  
+Clicking on `Get All Parks` button will fetch data from 3rd party api and display information about US national parks.
+
+![screenshot](./assets/images/nationalparks.png)
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,6 @@
 'use strict'
 
 const parkEvents = require('./parks/events.js')
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
 
 $(() => {
   parkEvents.addHandlers()
